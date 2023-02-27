8aa762aa88928312e8c7630307c14f15a5b6e0d3
AATHITYAN MARIRAJ <114569274+AATHITYANMARIRAJ@users.noreply.github.com>
2023-02-27 20:18:05 +0530

    Add files via upload

---
+++ 6.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int f1(int a=1,string b="hi"){
+	cout<<"The value of a is "<<a<<"\n";
+	cout<<"The value of b is "<<b<<"\n";
+	cout<<"integer and string function is executed\n";
+	return 0;
+}
+
+int f1(int a=1,double b=2.4){
+	cout<<"The value of a is "<<a<<"\n";
+	cout<<"The value of b is "<<b<<"\n";
+	cout<<"integer and double function is executed\n";
+	return 0;
+}
+
+
+int main(){
+	int a;
+	string b;
+	f1(1,b);
+	
+	return 0;
+}
