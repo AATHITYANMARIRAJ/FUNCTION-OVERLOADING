110ff556b2a9535622c4954700f71cd39f9323c2
AATHITYAN MARIRAJ <114569274+AATHITYANMARIRAJ@users.noreply.github.com>
2023-02-28 16:38:15 +0530

    Add files via upload

---
+++ 9.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template <typename T>
+T addall(vector<T> list){
+	T count=0;
+	for(auto& elem: list){
+		count+=elem;
+	}
+	return count;
+}
+
+template <typename T>
+T addall(vector<T> list,vector<T> list1){
+	T count=0;
+	for(auto& elem: list){
+		count+=elem;
+	}
+	T count1=0;
+	for(auto& elem1: list1){
+		count1+=elem1;
+	}
+	T c;
+	c=count+count1;
+	return c;
+}
+
+
+int main(){
+	vector<int> intv={1,2,3,4,5};
+	vector<int> intv1={1,2,3,4,5};
+	vector<double> floatv={1.2,2.4,3.8};
+	cout<<addall(intv,intv1)<<"\n";
+	cout<<addall(floatv)<<"\n";
+	return 0;
+}
