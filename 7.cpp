459aa629e32eee94e7aa83f9223f8c320923ae24
AATHITYAN MARIRAJ <114569274+AATHITYANMARIRAJ@users.noreply.github.com>
2023-02-28 15:33:02 +0530

    Add files via upload

---
+++ 7.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+template <typename T>
+T max(T a){
+	cout<<"same\n";
+		return a;
+}
+template <typename T1,typename T2>
+T2 max(T1 a,T2 b){
+	    cout<<"different\n";
+		if(a>b){
+		return a;
+	}
+	else{
+		return b;
+	}
+}
+
+int main(){
+cout<<max(3.8)<<"\n";
+cout<<max(3,4.0)<<"\n";
+cout<<max(3,4.4)<<endl;
+return 0;
+}
