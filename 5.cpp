72fe1a03ea99cc2407f2db6c158f6c5c5ad9652e
AATHITYAN MARIRAJ <114569274+AATHITYANMARIRAJ@users.noreply.github.com>
2023-02-28 22:56:36 +0530

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -14,30 +14,13 @@ double f1(double a,double b){
 	cout<<"MULTIPLICATION FUNCTION IS CALLED\n";
 	return a*b;
 }
+
+void f1(){
+	cout<<"integer and double func\n";
+}
 int main(){
-	cout<<"1-ADDITION\n";
-	cout<<"2-MULTIPLICATION\n";
-	int ch;
-	cout<<"enter your choice\n";
-	cin>>ch;
-	
-	if(ch==1){
-		int a,b;
-		cout<<"enter the first number\n";
-		cin>>a;
-		cout<<"enter the second number\n";
-		cin>>b;
-		int a1=f1(a,b);
-		cout<<a1<<"\n";
-	}
-	else{
-		double a,b;
-		cout<<"enter the first number\n";
-		cin>>a;
-		cout<<"enter the second number\n";
-		cin>>b;
-		double a2=f1(a,b);
-		cout<<a2<<"\n";
-	}
-	return 0;
+   f1();
+   f1(1,2);
+   f1(1.2,2.4);
+   return 0;
 }
